21079de10532e0b4397f674862562dfb7fc78ea9
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-23 21:24:21 +0300

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(result){
   // Write your code here!
+  let sample;
+  if(result <=400){
+    return "This one is on me!"
+  }
+  else if(result > 400 && result<=2000){
+    return "That will be twenty bucks."
+  }
+  else if (result >2000 && result < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (result >=2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  }
+  else{
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    break
+    default:
+      return "Bye."
+  }
 }
